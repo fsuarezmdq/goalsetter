8166dda9505627ea686d773bdade43624d0f0dee
Fernando Ariel Suarez <fernando.a.suarez@accenture.com>
2020-12-02 14:43:07 -0300

    Added RentalCommandTest

--- Goalsetter/Goalsetter.Tests/MockedData.cs
+++ Goalsetter/Goalsetter.Tests/MockedData.cs
@@ -17,6 +17,16 @@ namespace Goalsetter.Tests
         public static Vehicle Vehicle =
             Vehicle.Create((VehicleMakes) "Ford", (VehicleModel) "F-100", 2012, (Price) 80).Value;
 
+        public static Vehicle VehicleRemoved
+        {
+            get
+            {
+                var vehicle = Vehicle.Create((VehicleMakes) "Ford", (VehicleModel) "F-100", 2012, (Price) 80).Value;
+                vehicle.Remove();
+                return vehicle;
+            }
+        }
+
         public static DateRange DateRange = DateRange.Create(new DateTime(2020,1,1), new DateTime(2020,1,15)).Value;
 
         public static Rental Rental =
--- Goalsetter/Goalsetter.Tests/Services/RentalCommandsTest.cs
+++ Goalsetter/Goalsetter.Tests/Services/RentalCommandsTest.cs
@@ -5,7 +5,10 @@ using Goalsetter.Domains;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Goalsetter.Domains.ValueObjects;
 
 namespace Goalsetter.Tests.Services
 {
@@ -23,31 +26,60 @@ namespace Goalsetter.Tests.Services
             _mockClientRepository = new Mock<IClientRepository>();
             _mockRentalRepository = new Mock<IRentalRepository>();
 
-            _mockVehicleRepository.Setup(p => p.GetByIdAsync(It.IsAny<Guid>()))
-                .ReturnsAsync(MockedData.Vehicle);
-            _mockClientRepository.Setup(p => p.GetByIdAsync(It.IsAny<Guid>()))
-                .ReturnsAsync(MockedData.Client);
             _mockRentalRepository.Setup(p => p.Add(It.IsAny<Rental>()));
-                
+
         }
 
         [TestMethod]
         public async Task AddRentalCommandAsync()
         {
-
-            var clientId = MockedData.Rental.Client.Id;
-            var vehicleId = MockedData.Rental.Vehicle.Id;
             var dateRange = MockedData.Rental.DateRange;
 
-            //var command = 
+            _mockVehicleRepository.Setup(p => p.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(MockedData.Vehicle);
+            _mockClientRepository.Setup(p => p.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(MockedData.Client);
+            
             var commandHandler = new AddRentalCommand.AddRentalCommandHandler(_mockUnitOfWork.Object,
                 _mockRentalRepository.Object, _mockClientRepository.Object, _mockVehicleRepository.Object);
             
             var result = await commandHandler.Handle(
-                new AddRentalCommand(clientId, vehicleId, dateRange.StartDate, dateRange.EndDate));
+                new AddRentalCommand(MockedData.Rental.Client.Id, MockedData.Rental.Vehicle.Id, dateRange.StartDate, 
+                    dateRange.EndDate));
 
             Assert.IsTrue(result.IsSuccess);
         }
 
+        public static IEnumerable<object[]> TestParameters
+        {
+            get
+            {
+                yield return new object[] { MockedData.Vehicle, (Client)null, new DateTime(2020, 1, 1), new DateTime(2020, 1, 15), "The selected client does not exists." };
+                yield return new object[] { (Vehicle)null, MockedData.Client, new DateTime(2020, 1, 1), new DateTime(2020, 1, 15), "The selected vehicle does not exists." };
+                yield return new object[] { MockedData.Vehicle, MockedData.Client, new DateTime(2020, 1, 15), new DateTime(2020, 1, 1), "Start Date should be less than End Date" };
+                yield return new object[] { MockedData.VehicleRemoved, MockedData.Client, new DateTime(2020, 1, 1), new DateTime(2020, 1, 15),
+                    "Could not create the Rental. Vehicle should be active to create a rental. " };
+            }
+        }
+
+        [DataTestMethod]
+        [DynamicData("TestParameters")]
+        public async Task AddRentalCommandAsync_NonexistentEntity(Vehicle vehicle, Client client, DateTime startDate, DateTime endDate, string error)
+        {
+            _mockVehicleRepository.Setup(p => p.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(vehicle);
+            _mockClientRepository.Setup(p => p.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(client);
+            
+
+            var commandHandler = new AddRentalCommand.AddRentalCommandHandler(_mockUnitOfWork.Object,
+                _mockRentalRepository.Object, _mockClientRepository.Object, _mockVehicleRepository.Object);
+
+            var result = await commandHandler.Handle(
+                new AddRentalCommand(MockedData.Client.Id, MockedData.Vehicle.Id, startDate, endDate));
+
+            Assert.IsTrue(result.IsFailure);
+            Assert.AreEqual(error,result.Error);
+        }
     }
 }
